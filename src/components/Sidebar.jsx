64da470402620e11a7bd94f78f04b2ba7fd7821c
vramasrikanth14 <vramasrikanth@gmail.com>
2026-07-16 10:58:40 +0530

    bugs

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -157,7 +157,7 @@ export default function Sidebar({ user, collapsed, setCollapsed }) {
               </li>
             );
           })}
-        </ul>
+        </ul> 
       </nav>
 
       {/* FOOTER */}
